b52d828e4fb29afbb395a13ad4e9a2cce0bcd6c7
Your Name <cauanlima5@gmail.com>
2021-05-06 13:29:04 -0300

    fix(master):react_projeto_1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [lista, setLista] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <input type="text" onChange={(event) => setName(event.target.value)} />
+      <button onClick={() => setLista([ ...lista,name])}>Adicionar</button>
+      
+      
+      <ul>
+        {lista.map((item, i) => {
+          return <li>{item}
+          <button onClick={() => setLista(lista.filter((value,index) => index !== i))}>Remover</button>
+          <button onClick={() => setLista (lista.map((value,index) => index !== i ? value:name))}>Alterar</button>
+          </li>;
+        })}
+      </ul>
+    </body>
+
+    /* <button
+onClick={() => {
+  // name !== "" ? setName("") : setName("Frango")
+  aberto === true ? setAberto(false) : setAberto(true);
+}}
+>
+{aberto === true ? "Desaparece" : "Aparece"}
+</button>
+<p> {aberto === true ? "frango" : ""} </p> */
+
+    // <div>
+    //   <form>
+    //     <label>
+    //     <p>Contador: {name} </p>
+    //       <input type="text" onChange={(event) => setName(event.target.value)} />
+    //     </label>
+    //   </form>
+    // </div>
   );
 }
 

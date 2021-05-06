d16943d8626b0cb60b9af71c4e2015b6a3fe1357
Your Name <cauanlima5@gmail.com>
2021-05-06 13:45:22 -0300

    fix(master):react_projeto_1

--- src/App.js
+++ src/App.js
@@ -8,15 +8,31 @@ function App() {
   return (
     <body>
       <input type="text" onChange={(event) => setName(event.target.value)} />
-      <button onClick={() => setLista([ ...lista,name])}>Adicionar</button>
-      
-      
+      <button onClick={() => setLista([...lista, name])}>Adicionar</button>
+
       <ul>
         {lista.map((item, i) => {
-          return <li>{item}
-          <button onClick={() => setLista(lista.filter((value,index) => index !== i))}>Remover</button>
-          <button onClick={() => setLista (lista.map((value,index) => index !== i ? value:name))}>Alterar</button>
-          </li>;
+          return (
+            <li>
+              {item}
+              <button
+                onClick={() =>
+                  setLista(lista.filter((value, index) => index !== i))
+                }
+              >
+                Remover
+              </button>
+              <button
+                onClick={() =>
+                  setLista(
+                    lista.map((value, index) => (index !== i ? value : name))
+                  )
+                }
+              >
+                Alterar
+              </button>
+            </li>
+          );
         })}
       </ul>
     </body>

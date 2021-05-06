4412c507f9ddaa4ca131136476fbf28a8fd882fa
Your Name <cauanlima5@gmail.com>
2021-05-06 14:47:40 -0300

    fix(master):react_projeto_1

--- src/App.js
+++ src/App.js
@@ -5,37 +5,41 @@ import React, { useState } from "react";
 function App() {
   const [name, setName] = useState("");
   const [lista, setLista] = useState([]);
-  const [count,setCount] = useState(0)
-  // Esse array deve conter dois atributos, [ { id: 1, name: "teste" } ], 
-  // o nome deve aparecer na lista e o id deve ser utilizado para deletar e alterar.
+  const [count, setCount] = useState(0);
+  const funcaoAdicionar = () => {
+    setCount(count + 1);
+    setLista([...lista, { id: count, name: name }]);
+  };
+
+  const funcaoDeletar = () => {
+    setLista(lista.filter((value, index) => index !== i));
+  };
+
+  const funcaoAlterar = () => {
+    setLista(
+      lista.map((value, index) =>
+        index !== i ? value : { ...lista, name: name }
+      )
+    );
+  };
+
   return (
     <body>
       <input type="text" onChange={(event) => setName(event.target.value)} />
-      <button onClick={() => {setCount(count+1)
-      setLista([...lista, {id:count, name:name}])}
-    }>Adicionar</button>
-{/* mover lógica do click para uma função externa do render. Isso para todos as ações de click e change. */}
+      <button
+        onClick={() => {
+          funcaoAdicionar;
+        }}
+      >
+        Adicionar
+      </button>
       <ul>
         {lista.map((item, i) => {
           return (
             <li>
               {item.name}
-              <button
-                onClick={() =>
-                  setLista(lista.filter((value, index) => index !== i))
-                }
-              >
-                Remover
-              </button>
-              <button
-                onClick={() =>
-                  setLista(
-                    lista.map((value, index) => (index !== i ? value : {...lista,name:name}))
-                  )
-                }
-              >
-                Alterar
-              </button>
+              <button onClick={() => funcaoDeletar}>Remover</button>
+              <button onClick={() => funcaoAlterar}>Alterar</button>
             </li>
           );
         })}
